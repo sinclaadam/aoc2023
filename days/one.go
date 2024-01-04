4073258609477daa2258fb7fe98588b52bb5670d
Adam Sinclair <adam.sinclair@gmail.com>
2024-01-04 20:55:57 +1300

    day one part two

--- days/one.go
+++ days/one.go
@@ -4,8 +4,11 @@ import (
 	"aoc2023/utilities"
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode"
 )
+
+const maxWindow = 5
 
 func Run() {
 	lines, err := utilities.ReadLines("data/input1a.txt")
@@ -18,7 +21,7 @@ func Run() {
 
 	total := 0
 	for _, line := range lines {
-		number := ExtractNumbers(line)
+		number := ExtractNumbersPartTwo(line)
 		num, err := strconv.Atoi(number)
 
 		if err != nil {
@@ -42,6 +45,42 @@ func ExtractNumbers(line string) string {
 				last = r
 			} else {
 				last = r
+			}
+		}
+	}
+
+	return fmt.Sprintf("%c%c", first, last)
+}
+
+func ExtractNumbersPartTwo(line string) string {
+	numbers := map[string]rune{"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7', "eight": '8', "nine": '9'}
+
+	var first rune
+	var last rune
+
+	for i := 0; i < len(line); i++ {
+		code := rune(line[i])
+
+		if unicode.IsNumber(code) {
+			if first == 0 {
+				first = code
+				last = code
+			} else {
+				last = code
+			}
+		} else {
+			window := min(len(line)-i, maxWindow)
+			slice := line[i : i+window]
+
+			for key, value := range numbers {
+				if strings.HasPrefix(slice, key) {
+					if first == 0 {
+						first = value
+						last = value
+					} else {
+						last = value
+					}
+				}
 			}
 		}
 	}
--- tests/one_test.go
+++ tests/one_test.go
@@ -26,3 +26,30 @@ func TestExtractNumbers(t *testing.T) {
 		})
 	}
 }
+
+func TestExtractNumbersPartTwo(t *testing.T) {
+	var tests = []struct {
+		input  string
+		answer string
+	}{
+		{"two1nine", "29"},
+		{"eightwothree", "83"},
+		{"abcone2threexyz", "13"},
+		{"xtwone3four", "24"},
+		{"4nineeightseven2", "42"},
+		{"zoneight234", "14"},
+		{"7pqrstsixteen", "76"},
+		{"twonine9vqdzneight", "28"},
+		{"9five2", "92"},
+	}
+
+	for _, tt := range tests {
+		testname := tt.input + "," + tt.answer
+		t.Run(testname, func(t *testing.T) {
+			ans := days.ExtractNumbersPartTwo(tt.input)
+			if ans != tt.answer {
+				t.Errorf("got %s, want %s", ans, tt.answer)
+			}
+		})
+	}
+}
